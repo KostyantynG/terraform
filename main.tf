363ae6232499190476968706ead4d0e03b93d235
kostyantyn <funyourmind@gmail.com>
2022-10-12 17:31:55 +0200

    specify AZ for each subnet

--- main.tf
+++ main.tf
@@ -23,8 +23,9 @@ resource "aws_vpc" "myvpc" {
 
 # Create a Public subnet in AZ1
 resource "aws_subnet" "private_1" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.0.0/24"
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
 
   tags = {
     "Name" = "Public subnet 1"
@@ -33,8 +34,9 @@ resource "aws_subnet" "private_1" {
 
 # Create a Private subnet in AZ1
 resource "aws_subnet" "public_1" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
 
   tags = {
     "Name" = "Private subnet 1"
@@ -43,8 +45,9 @@ resource "aws_subnet" "public_1" {
 
 # Create a Public subnet in AZ2
 resource "aws_subnet" "private_2" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
 
   tags = {
     "Name" = "Public subnet 2"
@@ -53,8 +56,9 @@ resource "aws_subnet" "private_2" {
 
 # Create a Private subnet in AZ2
 resource "aws_subnet" "public_2" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-west-2b"
 
   tags = {
     "Name" = "Private subnet 2"
