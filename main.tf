caecaeced53a946158bf080df27b65565f80f226
kostyantyn <funyourmind@gmail.com>
2022-10-13 14:21:14 +0200

    add vpc.tf and userdata.sh files, modify main.tf file

--- main.tf
+++ main.tf
@@ -11,31 +11,3 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-
-# Create an Amazon linux 2 EC2 instance 
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-
-}
-
-resource "aws_instance" "test" {
-
-  ami                         = data.aws_ami.amazon-linux-2.id
-  associate_public_ip_address = true
-  instance_type               = "t2.micro"
-
-  tags = {
-    "Name" = "Terra server"
-  }
-}
-
--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "myvpc" {
 }
 
 # Create a Public subnet in AZ1
-resource "aws_subnet" "private_1" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-west-2a"
@@ -19,7 +19,7 @@ resource "aws_subnet" "private_1" {
 }
 
 # Create a Private subnet in AZ1
-resource "aws_subnet" "public_1" {
+resource "aws_subnet" "private__subnet_1" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_1" {
 }
 
 # Create a Public subnet in AZ2
-resource "aws_subnet" "private_2" {
+resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2b"
@@ -41,12 +41,78 @@ resource "aws_subnet" "private_2" {
 }
 
 # Create a Private subnet in AZ2
-resource "aws_subnet" "public_2" {
+resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-west-2b"
 
   tags = {
     "Name" = "Private subnet 2"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public RT"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_security_group" "ssh_security" {
+  name        = "allow_ssh"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Allow SSH"
+  }
+}
+
+resource "aws_instance" "first_instance" {
+  ami                         = "ami-019d533b7b2201eff"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids      = [aws_security_group.ssh_security.id]
+  key_name                    = "vockey"
+  user_data = file("userdata.sh")
+  tags = {
+    Name = "Suse instance"
   }
 }
