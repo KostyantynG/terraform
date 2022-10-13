2a2cb0d56bde3f752b3c549b9c1fe2efa45a729b
kostyantyn <funyourmind@gmail.com>
2022-10-13 14:44:07 +0200

    modify userdata.sh to install and configure web swerver on ec2 instance

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cd /home/ec2-user
-echo "Hey there!" > test.txt
+echo "Update server"
+yum update -y
+echo "Install and configure web server"
+yum install -y httpd
+systemctl enable httpd
+systemctl start httpd
+echo '<html><h1>Hello From Your Web Server, Fellas!</h1></html>' > /var/www/html/index.html
