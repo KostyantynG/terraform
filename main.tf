e0538f87fb3ee12ff86f31e8befb5a16bf489064
kostyantyn <funyourmind@gmail.com>
2022-10-13 09:57:59 +0200

    add vpc.tf file and write code for vpc and subnets in it, delete same from main.tf

--- main.tf
+++ main.tf
@@ -9,86 +9,33 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region     = "us-west-2"
-}
-
-# Create a VPC
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    "Name" = "Terraform VPC"
-  }
-}
-
-# Create a Public subnet in AZ1
-resource "aws_subnet" "private_1" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    "Name" = "Public subnet 1"
-  }
-}
-
-# Create a Private subnet in AZ1
-resource "aws_subnet" "public_1" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    "Name" = "Private subnet 1"
-  }
-}
-
-# Create a Public subnet in AZ2
-resource "aws_subnet" "private_2" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-2b"
-
-  tags = {
-    "Name" = "Public subnet 2"
-  }
-}
-
-# Create a Private subnet in AZ2
-resource "aws_subnet" "public_2" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-west-2b"
-
-  tags = {
-    "Name" = "Private subnet 2"
-  }
+  region = "us-west-2"
 }
 
 # Create an Amazon linux 2 EC2 instance 
 data "aws_ami" "amazon-linux-2" {
- most_recent = true
+  most_recent = true
 
-filter {
-   name   = "owner-alias"
-   values = ["amazon"]
- }
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
 
-filter {
-   name   = "name"
-   values = ["amzn2-ami-hvm*"]
- }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 
 }
 
 resource "aws_instance" "test" {
 
- ami                         = "${data.aws_ami.amazon-linux-2.id}"
- associate_public_ip_address = true
- instance_type               = "t2.micro"
+  ami                         = data.aws_ami.amazon-linux-2.id
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
 
- tags = {
-   "Name" = "Terra server"
- }
+  tags = {
+    "Name" = "Terra server"
+  }
 }
 
---
+++ vpc.tf
@@ -0,0 +1,52 @@
+# Create a VPC
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "Terraform VPC"
+  }
+}
+
+# Create a Public subnet in AZ1
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    "Name" = "Public subnet 1"
+  }
+}
+
+# Create a Private subnet in AZ1
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    "Name" = "Private subnet 1"
+  }
+}
+
+# Create a Public subnet in AZ2
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    "Name" = "Public subnet 2"
+  }
+}
+
+# Create a Private subnet in AZ2
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    "Name" = "Private subnet 2"
+  }
+}
