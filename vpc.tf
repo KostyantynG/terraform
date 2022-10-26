d73af258281218f5402be67586f6504d5039ba60
kostyantyn <funyourmind@gmail.com>
2022-10-26 17:06:21 +0200

    add NAT Gateway and Elastic IP

--- vpc.tf
+++ vpc.tf
@@ -167,4 +167,21 @@ resource "aws_lb_listener" "test" {
   }
 }
 
+# Create Elastic IP
+resource "aws_eip" "nat_ip" {
+  vpc      = true
+}
 
+# Create NAT gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_ip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
