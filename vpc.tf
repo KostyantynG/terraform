b46b7451b055f19765c601b3e66dade7693deb82
kostyantyn <funyourmind@gmail.com>
2022-10-26 14:47:23 +0200

    add listener to loadbalancer

--- vpc.tf
+++ vpc.tf
@@ -151,3 +151,14 @@ resource "aws_lb_target_group_attachment" "test" {
   target_id        = aws_instance.first_instance.id
   port             = 80
 }
+
+resource "aws_lb_listener" "test" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
+}
