80761ce4c8fbe3d49a7dd3664e8fc3687dce4710
kostyantyn <funyourmind@gmail.com>
2022-10-26 14:22:22 +0200

    add health check to target group

--- vpc.tf
+++ vpc.tf
@@ -134,6 +134,16 @@ resource "aws_lb_target_group" "test" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path = "/"
+    port = 80
+    healthy_threshold = 6
+    unhealthy_threshold = 2
+    timeout = 2
+    interval = 5
+    matcher = "200"  # has to be HTTP 200 or fails
+  }
 }
 
 resource "aws_lb_target_group_attachment" "test" {
